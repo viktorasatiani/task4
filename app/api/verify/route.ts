b421d79f56f74d594d86f229a24e5166961f0a20
Viktor <152264740+viktorasatiani@users.noreply.github.com>
2026-02-06 18:54:37 +0400

    updated route

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -1,48 +1,19 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
+export async function POST(req: Request) {
   try {
     await connectMongoDB();
-    const { email } = await request.json();
-
+    const { email } = await req.json();
     const user = await User.findOneAndUpdate(
       { email },
       { isVerified: true },
       { new: true },
     ).select("_id");
     console.log("user from verify ", user);
-
-    const response = await fetch(
-      "https://api.emailjs.com/api/v1.0/email/send",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          service_id: process.env.SERVICE_ID!,
-          template_id: process.env.TEMPLATE_ID!,
-          user_id: process.env.USER_ID!,
-          template_params: {
-            from_email: "viktorasatiani77@gmail.com",
-            company: "ACME",
-            email: email,
-          },
-        }),
-      },
-    );
-
-    if (response.ok) {
-      return NextResponse.json({ user });
-    } else {
-      return NextResponse.json(
-        { error: "Failed to send email" },
-        { status: 500 },
-      );
-    }
+    return NextResponse.json({ user });
   } catch (error) {
-    return NextResponse.json({ error: "Error sending email" }, { status: 500 });
+    console.log(error);
   }
 }
